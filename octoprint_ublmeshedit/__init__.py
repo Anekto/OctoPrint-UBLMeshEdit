e51cde41ac71fc587a4402f14903cb9c343173ff
Anekto <53334178+Anekto@users.noreply.github.com>
2024-12-28 15:52:52 +0100

    some changes

--- octoprint_ublmeshedit/__init__.py
+++ octoprint_ublmeshedit/__init__.py
@@ -1,20 +1,12 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 
 class UBLMeshEditPlugin(octoprint.plugin.AssetPlugin,
 						octoprint.plugin.SettingsPlugin,
 						octoprint.plugin.SimpleApiPlugin,
-                        octoprint.plugin.TemplatePlugin):
+						octoprint.plugin.TemplatePlugin):
 
 	def __init__(self):
 		self.wait_mesh = False
@@ -37,15 +29,13 @@ class UBLMeshEditPlugin(octoprint.plugin.AssetPlugin,
 		)
 
 	def get_template_configs(self):
-	 	return [
-	 		dict(type="settings", custom_bindings=False)
-	 	]
+		return [
+			dict(type="settings", custom_bindings=False)
+		]
 
 	##~~ AssetPlugin mixin
 
 	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
 		return dict(
 			js=["js/ublmeshedit.js"],
 			css=["css/ublmeshedit.css"]
@@ -56,61 +46,64 @@ class UBLMeshEditPlugin(octoprint.plugin.AssetPlugin,
 	def get_api_commands(self):
 		return {'wait_command': []}
 
-	def on_api_command(self, command, data):
+	def on_api_command(self, command, _):
 		if command == 'wait_command':
 			self.wait_ok = True
 
-	def on_gcode_sending(self, comm, phase, cmd, cmd_type, gcode, subcode=None, tags=None, *args, **kwargs):
-		if cmd=='M420 V1 T1': self.wait_mesh = True
+	def on_gcode_sending(self, _, __, cmd, ___, ____, _____=None, ______=None, *_______, **________):
+		if cmd == 'M420 V1 T1':
+			self.wait_mesh = True
 		return None
 
-	def on_gcode_recieved(self, comm, line, *args, **kwargs):
-			if not self.wait_g29 or line.strip()=='wait' or line.strip()=='Not SD printing': return line # early out
+	def on_gcode_recieved(self, _, line, *__, **___):
+		if not self.wait_g29 or line.strip() == 'wait' or line.strip() == 'Not SD printing':
+			return line  # early out
 
-			if line.strip() == 'Mesh Bed Leveling has no data.':
-				self.mesh_data = None
+		if line.strip() == 'Mesh Bed Leveling has no data.':
+			self.mesh_data = None
+			self.wait_g29 = False
+			self.send_mesh_collected_event()
+		elif 'Measured points:' in line:
+			self.mesh_data = []  # initialize empty mesh data
+			self.g29_mesh_line = -1  # ready for row count
+		elif self.g29_mesh_line is not None and self.g29_mesh_line > -1:
+			if self.g29_mesh_line == 0:
+				parts = line.strip().split()
+				if len(parts) > 2 and parts[1] == 'points:':
+					size = len(parts) - 2
+					if size != self._settings.get_int(['grid_size']):
+						self._logger.info(f"Mesh size {size} doesn't match plugin setting {self._settings.get_int(['grid_size'])}, changing to match")
+						self._settings.set_int(['grid_size'], size)
+						self._settings.save(trigger_event=True)
+
+			try:
+				# Parse each line, handling potential leading/trailing spaces
+				parts = line.strip().split()
+				row_index = int(parts[0])
+				if row_index == self.g29_mesh_line:
+					values = [float(x) for x in parts[1:]]
+					self.mesh_data.append(values)
+					self.g29_mesh_line += 1
+			except (ValueError, IndexError):
+				self._logger.debug(f"Skipping line: {line.strip()}")
+
+			if self.g29_mesh_line >= self._settings.get_int(['grid_size']):
+				self._logger.info("Got all mesh data")
 				self.wait_g29 = False
+				self.g29_mesh_line = None
 				self.send_mesh_collected_event()
-			elif 'Measured points:' in line:
-				self.mesh_data = [] # initialize empty mesh data
-				self.g29_mesh_line = -1 #ready for row count
-			elif self.g29_mesh_line is not None and self.g29_mesh_line > -1:
-				if self.g29_mesh_line == 0:
-					parts = line.strip().split()
-					if len(parts) > 2 and parts[1] == 'points:':
-						size = len(parts) - 2
-						if size != self._settings.get_int(['grid_size']):
-							self._logger.info(f"Mesh size {size} doesn't match plugin setting {self._settings.get_int(['grid_size'])}, changing to match")
-							self._settings.set_int(['grid_size'], size)
-							self._settings.save(trigger_event=True)
 
-				try:
-					# Parse each line, handling potential leading/trailing spaces
-					parts = line.strip().split()
-					row_index = int(parts[0])
-					if row_index == self.g29_mesh_line:
-						values = [float(x) for x in parts[1:]]
-						self.mesh_data.append(values)
-						self.g29_mesh_line += 1
-				except (ValueError, IndexError):
-					self._logger.debug(f"Skipping line: {line.strip()}")
+		elif self.g29_mesh_line == -1:
+			try:
+				self.g29_mesh_line = int(line[:line.find(':')])
+			except:
+				pass
 
-				if self.g29_mesh_line >= self._settings.get_int(['grid_size']):
-					self._logger.info("Got all mesh data")
-					self.wait_g29 = False
-					self.g29_mesh_line = None
-					self.send_mesh_collected_event()
+		return line
 
-			elif self.g29_mesh_line == -1:
-				try:
-					self.g29_mesh_line = int(line[:line.find(':')])
-				except:
-					pass
-
-			return line
-			
-	def on_atcommand_sending(self, comm, phase, cmd, params, tags=None, *args, **kwargs):
-		if cmd == 'UBLMESHEDIT': self.wait_mesh = True
+	def on_atcommand_sending(self, _, __, cmd, ___, ____=None, *_____, **______):
+		if cmd == 'UBLMESHEDIT':
+			self.wait_mesh = True
 
 	def send_command_complete_event(self):
 		event = octoprint.events.Events.PLUGIN_UBLMESHEDIT_COMMAND_COMPLETE
@@ -122,18 +115,16 @@ class UBLMeshEditPlugin(octoprint.plugin.AssetPlugin,
 			data = {'result': 'no mesh'}
 		else:
 			data = {'result': 'ok', 'data': self.mesh_data, 'gridSize': len(self.mesh_data), 'saveSlot': self.slot_num}
-		if self.not_ubl: data['notUBL'] = True
+		if self.not_ubl:
+			data['notUBL'] = True
 		self._event_bus.fire(event, payload=data)
 
-	def register_custom_events(*args, **kwargs):
+	def register_custom_events(*_, **__):
 		return ["mesh_ready", "command_complete"]
 
 	##~~ Softwareupdate hook
 
 	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
-		# for details.
 		return dict(
 			ublmeshedit=dict(
 				displayName="UBL Mesh Editor",
@@ -141,12 +132,12 @@ class UBLMeshEditPlugin(octoprint.plugin.AssetPlugin,
 
 				# version check: github repository
 				type="github_release",
-				user="The-EG",
+				user="Anekto",
 				repo="OctoPrint-UBLMeshEdit",
 				current=self._plugin_version,
 
 				# update method: pip
-				pip="https://github.com/The-EG/OctoPrint-UBLMeshEdit/archive/{target_version}.zip",
+				pip="https://github.com/Anekto/OctoPrint-UBLMeshEdit/archive/{target_version}.zip",
 
 				# release channels
 				stable_branch=dict(
@@ -165,17 +156,8 @@ class UBLMeshEditPlugin(octoprint.plugin.AssetPlugin,
 		)
 
 
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "UBL Mesh Editor"
-
-# Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
-# Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
-# compatibility flags according to what Python versions your plugin supports!
-#__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
-#__plugin_pythoncompat__ = ">=3,<4" # only python 3
-__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
+__plugin_pythoncompat__ = ">=2.7,<4"
 
 def __plugin_load__():
 	global __plugin_implementation__
--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "ublmeshedit"
+plugin_identifier = "ublmeshedit_FIX_MESH"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_ublmeshedit"
+plugin_package = "octoprint_ublmeshedit_FIX_MESH"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-UBLMeshEdit"
+plugin_name = "OctoPrint-UBLMeshEdit_FIX_MESH"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.3.2"
+plugin_version = "0.3.3"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
@@ -27,7 +27,7 @@ plugin_author = "Taylor Talkington"
 plugin_author_email = "taylor.talkington@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/The-EG/OctoPrint-UBLMeshEdit"
+plugin_url = "https://github.com/Anekto/OctoPrint-UBLMeshEdit"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
